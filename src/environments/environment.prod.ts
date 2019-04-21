443d6fa752cb41e3df724997dfea10cff357b9fe
asadzaidia <asadzaidi625@gmail.com>
2019-04-21 18:17:09 +0500

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB6yD6CxJ4rDtK3c2rMdOoE57LRSHKxi6g',
+    authDomain: 'fitness-tracker-625.firebaseapp.com',
+    databaseURL: 'https://fitness-tracker-625.firebaseio.com',
+    projectId: 'fitness-tracker-625',
+    storageBucket: 'fitness-tracker-625.appspot.com',
+    messagingSenderId: '561793202982'
+  }
 };
